a5a7fb5546ac70910d69ac364b2f91ce67a23e42
suryanshutomar <suryanshutomar123@outlook.com>
2023-09-28 11:05:26 +0530

    add: Header Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { signOut } from "next-auth/react";
+import styles from "./index.module.css";
+
+export default function Header() {
+	return (
+		<div className={styles.header}>
+			<div className={styles.appName}>Next Dash</div>
+			<button className={styles.signOutButton} onClick={() => signOut()}>
+				Sign Out
+			</button>
+		</div>
+	);
+}
