077d51f4abf297da0bcb07f74119a64726434057
suryanshutomar <suryanshutomar123@outlook.com>
2023-09-28 10:17:21 +0530

    remove: add custom global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import styles from "./page.module.css";
-
 export default function Home(): JSX.Element {
-	return <main className={styles.main}>Hello World!</main>;
+	return <main>Hello World!</main>;
 }
