272799bd4e277b2d74cfd0bcc31af45a2fd7c506
suryanshutomar <suryanshutomar123@outlook.com>
2023-09-28 10:42:16 +0530

    add: login card

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,13 @@
+import styles from "./page.module.scss";
+
 export default function LoginPage() {
-    return <div>Login Page</div>
+	return (
+		<div className={styles.loginContainer}>
+			<div className={styles.loginCard}>
+				<h1>My App</h1>
+				<p>Please log in to access your account.</p>
+				<button className={styles.loginButton}>Login</button>
+			</div>
+		</div>
+	);
 }
